8e0ea677dee31daf621fb0844f7d270e8bb4cadf
Jai Soni <Jai.Soni@mindbodyonline.com>
2022-08-27 17:32:07 +0530

    changes

--- ReminderServiceProblem/ReminderServiceProblem.Api/Controllers/ProcessTaskReminderController.cs
+++ ReminderServiceProblem/ReminderServiceProblem.Api/Controllers/ProcessTaskReminderController.cs
@@ -4,21 +4,21 @@ using ReminderServiceProblem.Domain;
 
 namespace ReminderServiceProblem.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProcessTaskReminderController : ControllerBase
-    {
-        private readonly ITaskReminderProcessor _taskReminderProcessor;
-        public ProcessTaskReminderController(ITaskReminderProcessor taskReminderProcessor)
-        {
-            _taskReminderProcessor = taskReminderProcessor;
-        }
+    //[Route("api/[controller]")]
+    //[ApiController]
+    //public class ProcessTaskReminderController : ControllerBase
+    //{
+    //    private readonly ITaskReminderProcessor _taskReminderProcessor;
+    //    public ProcessTaskReminderController(ITaskReminderProcessor taskReminderProcessor)
+    //    {
+    //        _taskReminderProcessor = taskReminderProcessor;
+    //    }
 
-        [HttpGet("process")]
-        public async Task<IActionResult> ProcessTaskReminders()
-        {
-            var result = await _taskReminderProcessor.GetAllTasks();
-            return Ok(result);
-        }
-    }
+    //    [HttpGet("process")]
+    //    public async Task<IActionResult> ProcessTaskReminders()
+    //    {
+    //        var result = await _taskReminderProcessor.GetAllTasks();
+    //        return Ok(result);
+    //    }
+    //}
 }
